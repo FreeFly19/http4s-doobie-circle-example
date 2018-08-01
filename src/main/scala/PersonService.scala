89839957853c1aed3441dc45e79464d51c699463
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-02 02:27:18 +0300

    Change circle encoding/decoding approach from semiauto to auto

--- src/main/scala/PersonService.scala
+++ src/main/scala/PersonService.scala
@@ -1,6 +1,5 @@
 import cats.effect.IO
-import io.circe._
-import io.circe.generic.semiauto._
+import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
@@ -8,8 +7,6 @@ import org.http4s.dsl.io._
 
 class PersonService {
   case class Human(name: String, age: Int)
-  implicit val humanDecoder: Decoder[Human] = deriveDecoder
-  implicit val humanEncoder: Encoder[Human] = deriveEncoder
 
   val service: HttpService[IO] = HttpService[IO]{
     case req @ POST -> Root => Ok(req.decodeJson[Human].map(_.asJson))
