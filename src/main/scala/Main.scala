4ed6b075fdfa754ed024200a1a2840bb737e55f4
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-02 01:50:12 +0300

    Adjust all endpoints to work with JSON

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import cats.effect._
 import fs2.StreamApp.ExitCode
 import fs2.{Stream, StreamApp}
+import io.circe.syntax._
 import io.circe.literal._
 import org.http4s._
 import org.http4s.circe._
@@ -13,9 +14,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Main extends StreamApp[IO] {
 
   val service = HttpService[IO] {
-    case GET -> Root => Ok("Home Page")
+    case GET -> Root => Ok("Home Page".asJson)
     case GET -> Root / "name" / name => Ok(json"""{"name": $name}""")
-    case _ -> url => NotFound(s"Sorry, but there is no handler for $url url!")
+    case _ -> url => NotFound(s"Sorry, but there is no handler for $url url!".asJson)
   }
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
