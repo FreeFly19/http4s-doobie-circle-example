60ee76bb569dcd9eb55f3f5cbd1ab4fecee3f429
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-02 02:11:56 +0300

    Create person service with JSON request parsing example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,10 @@
 import cats.effect._
 import fs2.StreamApp.ExitCode
 import fs2.{Stream, StreamApp}
-import io.circe.syntax._
+import io.circe.generic.semiauto._
 import io.circe.literal._
+import io.circe.syntax._
+import io.circe.{Decoder, Encoder}
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.io._
@@ -19,9 +21,18 @@ object Main extends StreamApp[IO] {
     case _ -> url => NotFound(s"Sorry, but there is no handler for $url url!".asJson)
   }
 
+  case class Human(name: String, age: Int)
+  implicit val humanDecoder: Decoder[Human] = deriveDecoder
+  implicit val humanEncoder: Encoder[Human] = deriveEncoder
+
+  val personService = HttpService[IO] {
+    case req @ POST -> Root => Ok(req.decodeJson[Human].map(_.asJson))
+  }
+
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] =
     BlazeBuilder[IO]
       .bindHttp(9090, "0.0.0.0")
       .mountService(service, "/")
+      .mountService(personService, "/api/persons")
       .serve
 }
