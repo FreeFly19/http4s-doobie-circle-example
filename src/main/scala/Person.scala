195a86057a9d07c9a8c5b918cccdda904ed8fd88
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-02 03:44:12 +0300

    Refactor findAll method in more reactive way

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,28 +1,35 @@
 import cats.effect._
+import fs2.Stream
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.io._
+import org.http4s.headers.`Content-Type`
+
 
 case class Person(id: Long, name: String, age: Int)
 case class CreatePersonCommand(name: String, age: Int)
 
 class PersonService(personRepository: PersonRepository[IO]) {
   val service: HttpService[IO] = HttpService[IO]{
-    case req @ POST -> Root => Ok {
+    case req @ POST -> Root => Ok(
       req
         .decodeJson[CreatePersonCommand]
         .flatMap(personRepository.save)
         .map(_.asJson)
-    }
-    case GET -> Root => Ok(personRepository.findAll.map(_.asJson))
+    )
+    case GET -> Root => Ok(
+      Stream("[") ++
+        personRepository.findAll.map(_.asJson.noSpaces).intersperse(",") ++
+        Stream("]"),
+      `Content-Type`(MediaType.`application/json`))
   }
 }
 
 trait PersonRepository[F[_]] {
   def save(p: CreatePersonCommand): F[Person]
-  def findAll: F[List[Person]]
+  def findAll: Stream[IO, Person]
 }
 
 class InMemoryPersonRepository extends PersonRepository[IO] {
@@ -38,6 +45,6 @@ class InMemoryPersonRepository extends PersonRepository[IO] {
     IO.pure(person)
   }
 
-  override def findAll: IO[List[Person]] =
-    IO.pure(persons)
+  override def findAll: Stream[IO, Person] =
+    Stream.fromIterator[IO, Person](persons.iterator)
 }
